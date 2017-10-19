43a92259f0fb52715335bfd1e9bfc4e3a13cc733
greentea <741024920@qq.com>
2017-10-19 17:47:36 +0800

    food 1-1

---
+++ src/components/food/food.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page">
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+
+      };
+    },
+    components: {
+
+    }
+  };
+</script>
+
+<style scoped lang="stylus">
+  .food
+    position fixed
+    left 0
+    top 0
+    bottom 48px
+    z-index 30
+    width 100%
+    background #fff
+
+</style>
