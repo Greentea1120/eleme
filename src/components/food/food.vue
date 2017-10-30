d63a1b00896bc06f61fef550bfbf0a9d9286731f
greentea <741024920@qq.com>
2017-10-30 17:39:14 +0800

    8-13~8-17

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -33,18 +33,20 @@
           <ratingselect @select="selectRating" @toggle="toggleContent" :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
           <div class="rating-wrapper">
             <ul v-show="food.ratings && food.ratings.length">
-              <li v-for="rating in food.ratings" class="rating-item">
+              <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
                 <div class="user">
-                  <span class="name">{{ratings.username}}</span>
+                  <span class="name">{{rating.username}}</span>
                   <img class="avatar" width="12" height="12" :src="rating.avatar" alt="">
                 </div>
-                <div class="time">{{rating.rateTime}}</div>
+                <div class="time">{{rating.rateTime | formatDate}}</div>
                 <p class="text">
-                  <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>
+                  <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
                 </p>
               </li>
             </ul>
-            <div class="no-rating" v-show="!food.ratings || !food.ratings"></div>
+            <div class="no-rating" v-show="!food.ratings || !food.ratings.length">
+              暂无评价
+            </div>
           </div>
         </div>
       </div>
@@ -58,6 +60,8 @@
   import cartcontrol from '@/components/cartcontrol/cartcontrol';
   import ratingselect from '@/components/ratingselect/ratingselect';
   import split from '@/components/split/split';
+
+  import {formatDate} from 'common/js/date';
 
   // const POSITIVE = 0;
   // const NEGATIVE = 1;
@@ -103,6 +107,22 @@
         this.$nextTick(() => {
           this.scroll.refresh();
         });
+      },
+      needShow(type, text) {
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let data = new Date(time);
+        return formatDate(data, 'yyyy-MM-dd hh:mm');
       }
     },
     data() {
@@ -126,6 +146,8 @@
 </script>
 
 <style scoped lang="stylus">
+  @import "../../common/stylus/mixin.styl";
+
   .food
     position fixed
     left 0
@@ -226,5 +248,46 @@
         margin-bottom 6px
         font-size 14px
         color rgb(7,17,27)
+      .rating-wrapper
+        padding 0 18px
+        .rating-item
+          position relative
+          padding 16px 0
+          border-1px(rgba(7,17,27,.1))
+          .user
+            position absolute
+            right 0
+            top 16px
+            font-size 0
+            line-height 12px
+            .name
+              display inline-block
+              margin-right 6px
+              vertical-align top
+              font-size 10px
+              color rgb(147,153,159)
+            .avatar
+              border-radius 50%
+          .time
+            margin-bottom 6px
+            line-height 12px
+            font-size 10px
+            color rgb(147,153,159)
+          .text
+            line-height 16px
+            font-size 12px
+            color rgb(7,17,27)
+            .icon-thumb_up,.icon-thumb_down
+              margin-right 4px
+              line-height 16px
+              font-size 12px
+            .icon-thumb_up
+              color rgb(0,160,220)
+            .icon-thumb_down
+              color rgb(147,153,159)
+        .no-rating
+          padding 16px 0
+          font-size 12px
+          color rgb(147,153,159)
 
 </style>
